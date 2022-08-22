45b0c1715cad15738e1e79be4afdffb086709595
packchi203 <bachnvth2108029@fpt.edu.vn>
2022-08-22 14:19:23 +0700

    a

--- cobid2/login/config.php
+++
@@ -1,9 +0,0 @@
-
-<?php
- $dataHost = 'localhost';
- $dataName = 'website_demo';
- $dataUserName = 'root';
- $dataPassword = '';
- 
- $mysqli = mysqli_connect($dataHost,$dataUserName,$dataPassword, $dataName)
-?>
